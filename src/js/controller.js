39b951c71e0b25b3dad9ada4cb0a4e61b29e39b7
fadayopaul <paulfadayo@yahoo.com>
2023-02-07 23:07:59 +0100

    rendering recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,7 +28,7 @@ const showRecipe = async () => {
       id: recipe.id,
       title: recipe.title,
       publisher: recipe.publisher,
-      SourceuRL: recipe.source_url,
+      sourceuRL: recipe.source_url,
       image: recipe.image_url,
       servings: recipe.servings,
       cookingTime: recipe.cooking_time,
@@ -39,9 +39,9 @@ const showRecipe = async () => {
     // 2.) Rendering recipe
     const markup = `
         <figure class="recipe__fig">
-          <img src="src/img/test-1.jpg" alt="Tomato" class="recipe__img" />
+          <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
           <h1 class="recipe__title">
-            <span>Pasta with tomato cream sauce</span>
+            <span>${recipe.title}</span>
           </h1>
         </figure>
 
@@ -50,14 +50,14 @@ const showRecipe = async () => {
             <svg class="recipe__info-icon">
               <use href="src/img/icons.svg#icon-clock"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">45</span>
+            <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
             <span class="recipe__info-text">minutes</span>
           </div>
           <div class="recipe__info">
             <svg class="recipe__info-icon">
               <use href="src/img/icons.svg#icon-users"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--people">4</span>
+            <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
             <span class="recipe__info-text">servings</span>
 
             <div class="recipe__info-buttons">
@@ -117,12 +117,12 @@ const showRecipe = async () => {
           <h2 class="heading--2">How to cook it</h2>
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
-            <span class="recipe__publisher">The Pioneer Woman</span>. Please check out
+            <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
             directions at their website.
           </p>
           <a
             class="btn--small recipe__btn"
-            href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
+            href="${recipe.sourceuRL}"
             target="_blank"
           >
             <span>Directions</span>
@@ -131,7 +131,9 @@ const showRecipe = async () => {
             </svg>
           </a>
         </div>
+
     `;
+    recipeContainer.insertAdjacentHTML("beforeend", markup);
   } catch (error) {
     console.error(`${error}`);
   }
