a002fa8d9d16abeebe6669b2f537f489deb5b674
fadayopaul <paulfadayo@yahoo.com>
2023-02-12 00:15:49 +0100

    create helper file

---
+++ src/js/helpers.js
@@ -0,0 +1,14 @@
+import { async } from "regenerator-runtime";
+
+export const getJSON = async function (url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
