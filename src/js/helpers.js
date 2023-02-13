07e565355a8f65e4c6fa82761f07698ab5e1331b
fadayopaul <paulfadayo@yahoo.com>
2023-02-13 20:12:42 +0100

    implement the timout in the code

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,15 +1,26 @@
 import { async } from "regenerator-runtime";
+import { TIMEOUT_SEC } from "./config.js";
+
+const timeout = function (second) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(
+        new Error(`Request took too long! Timeout after ${second} second`)
+      );
+    }, second * 1000);
+  });
+};
 
 export const getJSON = async function (url) {
   try {
-    const response = await fetch(url);
+    const fetchPro = fetch(url);
+    const response = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
     const data = await response.json();
 
     if (!response.ok) throw new Error(`${data.message} (${response.status})`);
 
     return data;
   } catch (error) {
-    throw new Error(error);
-    // console.log(error);
+    throw error;
   }
 };
