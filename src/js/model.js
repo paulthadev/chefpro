147fd78ce3a391cca5430d3dedaf9b5f4fad2ca9
fadayopaul <paulfadayo@yahoo.com>
2023-02-16 21:24:15 +0100

    implementing error, success essage

--- src/js/model.js
+++ src/js/model.js
@@ -1,6 +1,7 @@
 import { async } from "regenerator-runtime";
 import { API_URL } from "./config.js";
 import { getJSON } from "./helpers.js";
+import recipeViews from "./views/recipeViews.js";
 
 export const state = {
   recipe: {},
@@ -23,6 +24,6 @@ export const loadRecipe = async function (id) {
     };
     // console.log(recipe);
   } catch (error) {
-    console.error(`${error} 💥💥💥`);
+    throw error;
   }
 };
