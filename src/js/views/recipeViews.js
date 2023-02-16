7f6036dbf9a3a27341b7516089eec2a623007b27
fadayopaul <paulfadayo@yahoo.com>
2023-02-16 21:24:29 +0100

    implementing error, success essage

--- src/js/views/recipeViews.js
+++ src/js/views/recipeViews.js
@@ -5,6 +5,8 @@ import { Fraction } from "fractional";
 class RecipeView {
   #parentElement = document.querySelector(".recipe");
   #data;
+  #errorMessage = "We couldn't find that recipe. Please Try another one!";
+  #message = "";
 
   render(data) {
     this.#data = data;
@@ -25,7 +27,37 @@ class RecipeView {
         </svg>
       </div>
         `;
-    this.#parentElement.innerHTML = "";
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  renderError(message = this.#errorMessage) {
+    const markup = `
+          <div class="error">
+            <div>
+              <svg>
+                <use href="${icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>
+    `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  renderMessage(message = this.#message) {
+    const markup = `
+          <div class="message">
+            <div>
+              <svg>
+                <use href="${icons}#icon-smile"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>
+    `;
+    this.#clear();
     this.#parentElement.insertAdjacentHTML("afterbegin", markup);
   }
 
