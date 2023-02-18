5ad42603bec32d0b7d2e78a2878d793b00f09000
fadayopaul <paulfadayo@yahoo.com>
2023-02-18 23:23:13 +0100

    implement search results & error message

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -3,6 +3,8 @@ import Views from "./views";
 
 class resultsView extends Views {
   _parentElement = document.querySelector(".results");
+  _errorMessage = "No recipes found for your query! Please try again :)";
+  _message = "";
 
   _generateMarkup() {
     console.log(this._data);
@@ -12,19 +14,14 @@ class resultsView extends Views {
   _generateMarkupPreview(result) {
     return `
       <li class="preview">
-        <a class="preview__link preview__link--active" href="#${result.id}">
+        <a class="preview__link " href="#${result.id}">
             <figure class="preview__fig">
                 <img src="${result.image}" alt="Test" />
             </figure>
             <div class="preview__data">
                 <h4 class="preview__title">${result.title}</h4>
                 <p class="preview__publisher">${result.publisher}</p>
-                <div class="preview__user-generated">
-                  <svg>
-                    <use href=${icons}#icon-user"></use>
-                  </svg>
-                </div>
-              </div>
+            </div>
         </a>
       </li>
     `;
