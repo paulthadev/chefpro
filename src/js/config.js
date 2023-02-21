e5768635578fe8a888273798bc43212fd3729446
fadayopaul <paulfadayo@yahoo.com>
2023-02-21 05:15:49 +0100

    update configuration file

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
 
 ///////////////////////////////////////
 // https://forkify-api.herokuapp.com/v2
