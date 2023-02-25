346071e235abba7cc16af4c4b5b9337247e0f3cb
fadayopaul <paulfadayo@yahoo.com>
2023-02-25 08:45:07 +0100

    Add default page number

--- src/js/config.js
+++ src/js/config.js
@@ -2,6 +2,7 @@ export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const DEF_SERVINGS = 1;
+export const DEF_PAGE = 1;
 
 /// API DATABASE///////////////////
 // https://forkify-api.herokuapp.com/v2
