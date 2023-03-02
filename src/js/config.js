6178776306dae76ee90ff9981960db83c74cb6a9
fadayopaul <paulfadayo@yahoo.com>
2023-03-02 02:39:08 +0100

    send recipe to the API data

--- src/js/config.js
+++ src/js/config.js
@@ -3,6 +3,7 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const DEF_SERVINGS = 1;
 export const DEF_PAGE = 1;
+export const KEY = "c33b629b-af96-4bc6-8fde-ff339494844f";
 
 /// API DATABASE///////////////////
 // https://forkify-api.herokuapp.com/v2
