29c7e3b1a8b974aff83bbae88177c4101372cfbb
fadayopaul <paulfadayo@yahoo.com>
2023-03-08 02:59:13 +0100

    update configuaration file

--- src/js/config.js
+++ src/js/config.js
@@ -4,7 +4,7 @@ export const RES_PER_PAGE = 10;
 export const DEF_SERVINGS = 1;
 export const DEF_PAGE = 1;
 export const KEY = "d78e8f4c-7007-4169-84c7-989a329d8f04";
-export const MODAL_CLOSE_SEC = 2.5;
+export const MODAL_CLOSE_SEC = 1.5;
 
 /// API DATABASE///////////////////
 // https://forkify-api.herokuapp.com/v2
