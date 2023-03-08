91e4397a59502167ee39eca3893df9bd3172a740
fadayopaul <paulfadayo@yahoo.com>
2023-03-08 02:59:31 +0100

    add JSDocumentation

--- src/js/views/views.js
+++ src/js/views/views.js
@@ -3,6 +3,16 @@ import icons from "url:../../img/icons.svg";
 export default class Views {
   _data;
 
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Paul Fadayo
+   * @todo Finish implementation
+   */
+
   render(data, render = true) {
     this._data = data;
 
